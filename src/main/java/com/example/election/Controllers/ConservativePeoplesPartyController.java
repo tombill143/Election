63fcb9de34ab690a9e1c2df7a0a401fc9c418834
tombill143 <tombill143@gmail.com>
2021-12-20 17:26:12 +0100

    Uploading changes to the ConservativePeoplesPartyController class

--- src/main/java/com/example/election/Controllers/ConservativePeoplesPartyController.java
+++ src/main/java/com/example/election/Controllers/ConservativePeoplesPartyController.java
@@ -19,14 +19,16 @@ public class ConservativePeoplesPartyController {
 
     @GetMapping("/conservativecandidate")
     public String addConservativeCandidate(){
-      ConservativePeoplesParty candidate1 = new ConservativePeoplesParty(1L, "Marcel", "Meijer");
-      ConservativePeoplesParty candidate2 = new ConservativePeoplesParty(2L,  "Michael", "Kristensen");
-      ConservativePeoplesParty candidate3 = new ConservativePeoplesParty(3L, "Helle", "Hensen");
-      ConservativePeoplesParty candidate4 = new ConservativePeoplesParty(4L, "Karina", "Knobelauch");
-      ConservativePeoplesParty candidate5 = new ConservativePeoplesParty(5L, "Stefan Haffstein", "Wolffbrandt");
-      ConservativePeoplesParty candidate6 = new ConservativePeoplesParty(6L, "Robert V.", "Rasmussen");
-      ConservativePeoplesParty candidate7 = new ConservativePeoplesParty(7L, "Pia", "Ramsing");
-      ConservativePeoplesParty candidate8 = new ConservativePeoplesParty(8L, "Anders Baun", "Sørensen");
+      ConservativePeoplesParty candidate1 = new ConservativePeoplesParty(1L, "Urban", "Olsen");
+      ConservativePeoplesParty candidate2 = new ConservativePeoplesParty(2L, "Peter", "Askjær");
+      ConservativePeoplesParty candidate3 = new ConservativePeoplesParty(3L, "Martin", "Sørensen");
+      ConservativePeoplesParty candidate4 = new ConservativePeoplesParty(4L, "Louise", "Bramstorp");
+      ConservativePeoplesParty candidate5 = new ConservativePeoplesParty(5L, "Sigfred", "Jensen");
+      ConservativePeoplesParty candidate6 = new ConservativePeoplesParty(6L, "Jørn C. ", "Nissen");
+      ConservativePeoplesParty candidate7 = new ConservativePeoplesParty(7L, "Morten Ø. ", "Kristensen");
+      ConservativePeoplesParty candidate8 = new ConservativePeoplesParty(8L, "Susanne", "Andersen");
+      ConservativePeoplesParty candidate9 = new ConservativePeoplesParty(9L, "Iulain V. ", "Paiu");
+      ConservativePeoplesParty candidate10 = new ConservativePeoplesParty(10L, "Per", "Hingel" );
 
       conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate1);
       conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate2);
@@ -36,6 +38,8 @@ public class ConservativePeoplesPartyController {
       conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate6);
       conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate7);
       conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate8);
+      conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate9);
+      conservativePeoplesPartyService.getConservativePeoplesPartyRepo().save(candidate10);
 
       return "The Conservative Candidates mentioned in the controller should be now uploaded to the database";
     }
--- src/main/java/com/example/election/Controllers/SocialDemocratsController.java
+++ src/main/java/com/example/election/Controllers/SocialDemocratsController.java
@@ -1,5 +1,44 @@
 package com.example.election.Controllers;
 
+import com.example.election.Entitities.SocialDemocrats;
+import com.example.election.Services.SocialPeoplesPartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SocialDemocratsController {
+
+    @Autowired
+    SocialPeoplesPartyService socialPeoplesPartyService;
+
+    public SocialDemocratsController(SocialPeoplesPartyService socialPeoplesPartyService){
+
+    }
+
+    @GetMapping("/addsocialdemocratcandidate")
+  public String addSocialDemocratCandidate(){
+
+      SocialDemocrats candidate1 = new SocialDemocrats(1L, "Marcel", "Meijer");
+      SocialDemocrats candidate2 = new SocialDemocrats(2L, "Michael", "Kristensen");
+      SocialDemocrats candidate3 = new SocialDemocrats(3L, "Helle", "Hensen");
+      SocialDemocrats candidate4 = new SocialDemocrats(4L, "Karina", "Knobelauch");
+      SocialDemocrats candidate5 = new SocialDemocrats(5L, "Stefan Haffstein", "Wolffbrandt");
+      SocialDemocrats candidate6 = new SocialDemocrats(6L, "Robert V.", "Rasmussen");
+      SocialDemocrats candidate7 = new SocialDemocrats(7L, "Pia", "Ramsing");
+      SocialDemocrats candidate8 = new SocialDemocrats(8L, "Anders Baun", "Sørensen");
+
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+      socialPeoplesPartyService.getSocialDemocratsRepo().save(candidate1);
+
+
+      return "The Social Democrat Candidates mentioned in the controller should be now uploaded to the database";
+    }
 
 }
